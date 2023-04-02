eb6541f85a63395d546191e2b1cdfa60abc45233
Sohib <sohib19950133@gmail.com>
2023-04-02 14:25:05 +0500

    API server

--- js/script.js
+++ js/script.js
@@ -252,9 +252,8 @@ window.addEventListener('DOMContentLoaded', () => {
          
          form.insertAdjacentElement('afterend', statusMessage);
 
-         let request = new XMLHttpRequest();
-         request.open('POST', 'server.php');
-         request.setRequestHeader('Content-type', 'application/json');
+     
+         
 
          let formData = new FormData(form); 
 
@@ -264,24 +263,29 @@ window.addEventListener('DOMContentLoaded', () => {
           object[key] = value;
          });
 
-         let json = JSON.stringify(object);
-
-         request.send(json);
-
-         request.addEventListener('load', () => {
-          
-          if(request.status === 200){
-            console.log(request.response);
-            showThanksModal(message.success);
-            form.reset();
          
-              statusMessage.remove();
-         
-          } else {
-            showThanksModal(message.failure);
-          }
 
-         });
+         fetch('server.php', {
+          method: "POST",
+          headers:{
+            'Content-type': 'application/json'
+          },
+          body: JSON.stringify(object)
+        }).then(data => data.text())
+        
+        .then(data => {
+          console.log(data);
+          showThanksModal(message.success);
+     
+           
+          statusMessage.remove();
+        }).catch(() => {
+          showThanksModal(message.failure);
+        }).finally(() => {
+          form.reset();
+        })
+
+      
       });
     }
 
